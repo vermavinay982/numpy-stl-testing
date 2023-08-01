777391ce1a9b73b34a2a06afcd65b0103d5a7042
Vinay Kumar Verma <vermavinay982@gmail.com>
2023-08-02 00:13:14 +0530

    Added 2d plotting for stl

--- flat_rect.py
+++ flat_rect.py
@@ -9,35 +9,36 @@ vertices = np.array([\
     [+1, -1, -1], # x
     [+1, +1, -1], # x,y
     [-1, +1, -1], # y
-    [-1, -1, +1], # z
-    [+1, -1, +1], # x,z
-    [+1, +1, +1], # x,y,z
-    [-1, +1, +1]  # y,z
+    [+1, -1, -1], # z
+    [+2, -1, -1], # x,z
+    [+2, +1, -1], # x,y,z
+    [+1, +1, -1]  # y,z
+
     ])
 # Define the 12 triangles composing the cube
 faces = np.array([\
     [0,3,1],
     [1,3,2],
+    # [0,4,7],
+    # [0,7,3],
 
-    [0,4,7],
-    [0,7,3],
-
-    [4,5,6],
+    # [4,5,6],
     [4,6,7],
     
-    [5,1,2],
-    [5,2,6],
+    # [5,1,2],
+    # [5,2,6],
 
-    [2,3,6],
-    [3,7,6],
+    # [2,3,6],
+    # [3,7,6],
 
-    [0,1,5],
-    [0,5,4]
+    # [0,1,5],
+    # [0,5,4]
     ])
 
 
 
-cube_mesh = np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype)
+cube_mesh = np.ones(faces.shape[0])*0
+cube_mesh = np.array(cube_mesh,dtype=mesh.Mesh.dtype)
 # Create the mesh
 ic(cube_mesh)
 
@@ -46,10 +47,9 @@ for i, f in enumerate(faces):
     for j in range(3):
         # vert = vertices[f[j],:]
         vert = vertices[f[j]]
-
+        cube.vectors[i][j] = vert 
         ic(j, vert)
 
-        cube.vectors[i][j] = vert 
     print()
     # cube.save('cube.stl')
     # stl_file_path = "cube.stl"
@@ -57,6 +57,6 @@ for i, f in enumerate(faces):
     # mesh.show()
 
 # Write the mesh to file "cube.stl"
-cube.save('cube.stl')
+cube.save('flat_rect.stl')
 
 
